2863b85b9e97083fb8fce72248c056e2dbab1d1c
Daylon Crider <daylon.crider@gmail.com>
2018-09-27 09:23:34 -0700

    Login not working. Trying to fix

--- pages/home.php
+++ pages/home.php
@@ -1,8 +1,10 @@
 <?php
+/*
     if(!isset($_SESSION['is-logged-in'])){
         header('Location: pages/login.php');
         die();
     }
+*/
     echo "You'll be able to see the homepage soon enough!";
     die();
 ?>
