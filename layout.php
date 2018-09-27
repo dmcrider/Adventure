cc272abde4af948835e88966d600c79c2bbd8056
Daylon Crider <daylon.crider@gmail.com>
2018-09-27 10:13:31 -0700

    Layout updates

--- layout.php
+++ layout.php
@@ -8,29 +8,30 @@
 <body class="container-fluid">
     <!-- Header -->
     <div class="row top-row">
-        <div class="col-md-12" style="text-align:center;">
-            <p>Adventure</p>
-            <?php  // Only show the naviagation menu if the user is logged in ?>
-            <?php if(isset($_SESSION['current-user'])) : ?>
-                <div class="col-md-4 user">
-                    <button type="button" class="btn btn-primary outline dropdown-toggle" data-toggle="dropdown">
-                        <img src="_assets/img/glyph-user.png">
-                        <?php echo $_SESSION['current-user']->firstname ?>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <?php if(isset($_GET['action'])) : ?>
-                            <?php if($_GET['action'] != '') :  ?>
-                                <li class="dropdown-item"><a href="index.php?action=home"><img src="_assets/img/glyph-home.png"> Home</a></li>
-                            <?php endif; ?>
-                        <?php endif; ?>
-                        <li class="dropdown-item"><a href="index.php?action=account"><img src="_assets/img/glyph-vcard.png"> My Account</a></li>
-                        <li class="dropdown-item"><a href="index.php?action=logout"><img src="_assets/img/glyph-logout.png"> Logout</a></li>
-                    </ul>
-                </div>
-            <?php else : ?>
-                <div class="col-md-4"></div>
-            <?php endif; ?>
+        <div class="col-md-4 brand"><!-- Logo goes here --></div>
+        <div class="col-md-4 header">
+            <h1>Adventure</h1>
         </div>
+        <?php  // Only show the naviagation menu if the user is logged in ?>
+        <?php if(isset($_SESSION['current-user'])) : ?>
+            <div class="col-md-4 user">
+                <button type="button" class="btn btn-primary outline dropdown-toggle" data-toggle="dropdown">
+                    <img src="_assets/img/glyph-user.png">
+                    <?php echo $_SESSION['current-user']->firstname ?>
+                </button>
+                <ul class="dropdown-menu">
+                    <?php if(isset($_GET['action'])) : ?>
+                        <?php if($_GET['action'] != '') :  ?>
+                            <li class="dropdown-item"><a href="index.php?action=home"><img src="_assets/img/glyph-home.png"> Home</a></li>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                    <li class="dropdown-item"><a href="index.php?action=account"><img src="_assets/img/glyph-vcard.png"> My Account</a></li>
+                    <li class="dropdown-item"><a href="index.php?action=logout"><img src="_assets/img/glyph-logout.png"> Logout</a></li>
+                </ul>
+            </div>
+        <?php else : ?>
+            <div class="col-md-4"></div>
+        <?php endif; ?>
     </div>
     <!-- Body -->
     <div class="row">
